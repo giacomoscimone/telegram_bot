c8ef71d25ef5786a97d6302a25314414bacddc58
giacomoscimone <giacomo.scimone.it@gmail.com>
2024-07-17 16:14:21 +0200

    first commit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 from dotenv import load_dotenv
-
+import requests
 import telebot
 from telebot.types import Message
+import json
 
 load_dotenv()
 
@@ -13,13 +14,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 PATH = "C:\\Users\\Alternanza\\Documents\\GitHub\\telegram_bot\\"
 
 
-@bot.message_handler(commands=['carica_foto'])
-def foto(message: Message):
-    bot.reply_to(message, "seleziona la foto: ")
-    bot.register_next_step_handler(message=message, callback=upload_foto)
-
-
-def save_image(img,path):
+def save_image(img, path):
     with open(path, 'wb') as file:
         file.write(img)
 
@@ -30,6 +25,38 @@ def upload_foto(message: Message):
     downloaded_file = bot.download_file(file_info.file_path)
     save_image(downloaded_file, PATH + file_info.file_path[6:])
     bot.reply_to(message, "immagine caricata con successo")
+
+    classe, confidence = upload_server(file_info.file_path[6:])
+    if classe is not None:
+        bot.reply_to(message, f"E' una {classe}!\n(Sono sicuro al {round(confidence*100)}%)")
+
+
+def upload_server(file_path):
+
+    files = [
+        ('file', (file_path, open(PATH + file_path, 'rb'), 'image/jpeg'))
+    ]
+    response = requests.post('http://localhost:8000/upload-image/', files=files)
+
+    if response.status_code == 200:
+        print('Dati inviati con successo')
+
+        convert_dizionario = response.json()
+        message = convert_dizionario["message"]
+        
+        classe = message["class"]
+        confidence = message["confidence"]
+
+        return classe, confidence
+    else:
+        print('Errore nella richiesta')
+        return None
+
+
+@bot.message_handler(commands=['predizione'])
+def class_prediction(message: Message):
+    bot.reply_to(message, "seleziona la foto: ")
+    bot.register_next_step_handler(message=message, callback=upload_foto)
 
 
 bot.infinity_polling()
