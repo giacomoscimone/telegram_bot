c2318769c97ffe4fe5d5fce6ae83ac5d2c06dfeb
giacomoscimone <giacomo.scimone.it@gmail.com>
2024-07-18 12:17:02 +0200

    refactor server utils

--- server_utils.py
+++ server_utils.py
@@ -1,19 +1,28 @@
 import requests
-from costant_path import SAVE_PATH
+from constants.paths import SAVE_PATH
+import logging
+from constants.server import HOST_PATH, PORTA_SERVER, PREDICT_FUNCTION
+from io_utils import read_file
+
+logger = logging.getLogger('my_logger')
 
 
-def predict_server(file_path):
+def predict_server(file_path: str) -> (str, float):
+
+    file = read_file(SAVE_PATH + file_path)
 
     files = [
-        ('file', (file_path, open(SAVE_PATH + file_path, 'rb'), 'image/jpeg'))
+        ('file', (file_path, file, 'image/jpeg'))
     ]
-    response = requests.post('http://localhost:8000/upload-image/', files=files)
+    logger.debug("immagine caricata dal predict")
+    response = requests.post(HOST_PATH + PORTA_SERVER + PREDICT_FUNCTION, files=files)
+    logger.debug("immagine inviata al server")
 
     if response.status_code == 200:
-        print('Dati inviati con successo')
+        logger.info('Dati inviati con successo')
 
-        convert_dizionario = response.json()
-        message = convert_dizionario["message"]
+        message_dict = response.json()
+        message = message_dict["message"]
 
         classe = message["class"]
         confidence = message["confidence"]
