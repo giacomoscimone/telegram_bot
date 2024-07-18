f8db6beae759cf0614b9af749d4859a7e9f9a898
giacomoscimone <giacomo.scimone.it@gmail.com>
2024-07-18 12:16:45 +0200

    refactor main

--- main.py
+++ main.py
@@ -2,38 +2,51 @@ import os
 from dotenv import load_dotenv
 from server_utils import predict_server
 from telebot.types import Message
-from costant_path import SAVE_PATH, PATH, LOG_PATH
-from io_utils import create_save_location, save_image
+from constants.paths import SAVE_PATH, LOG_PATH
+from io_utils import create_save_location, save_image, clean_up
 import telebot
 from logger_utils import setuplog
+import logging
 
 load_dotenv()
+
+logger = logging.getLogger('my_logger')
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
-def upload_foto(message: Message):
+def upload_foto(message: Message) -> None:
     file_id = message.photo[-1].file_id
     file_info = bot.get_file(file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-    save_image(downloaded_file, PATH + file_info.file_path[6:])
-    bot.reply_to(message, "immagine caricata con successo")
+    img_path = file_info.file_path
 
-    classe, confidence = predict_server(file_info.file_path[6:])
+    downloaded_file = bot.download_file(img_path)
+    save_image(downloaded_file, SAVE_PATH + img_path[6:])
+
+    bot.reply_to(message, "immagine caricata con successo")
+    logger.info(f"immagine salvata in: {SAVE_PATH + img_path[6:]}")
+
+    classe, confidence = predict_server(img_path[6:])
     if classe is not None:
         bot.reply_to(message, f"E' una {classe}!\n(Sono sicuro al {round(confidence*100)}%)")
+    else:
+        logger.error("errore richiesta del server")
+        bot.reply_to(message, "errore server")
 
 
 @bot.message_handler(commands=['predizione'])
-def class_prediction(message: Message):
+def class_prediction(message: Message) -> None:
 
     bot.reply_to(message, "seleziona la foto: ")
+    logger.debug("risposta inviata")
     bot.register_next_step_handler(message=message, callback=upload_foto)
 
 
 if __name__ == "__main__":
     create_save_location(SAVE_PATH)
+    clean_up(SAVE_PATH)
     setuplog(LOG_PATH)
     bot.infinity_polling()
+
