8818a4669d43e2dcc62a8ec57cbfc6dab353da43
giacomoscimone <giacomo.scimone.it@gmail.com>
2024-07-19 10:01:24 +0200

    add resize function

--- server_utils.py
+++ server_utils.py
@@ -1,7 +1,7 @@
 import requests
 from constants.paths import SAVE_PATH
 import logging
-from constants.server import HOST_PATH, PORTA_SERVER, PREDICT_FUNCTION, GRAYSCALE_FUNCTION
+from constants.server import HOST_PATH, PORTA_SERVER, PREDICT_FUNCTION, GRAYSCALE_FUNCTION, RESIZE_FUNCTION
 from io_utils import read_file
 
 logger = logging.getLogger('my_logger')
@@ -15,7 +15,7 @@ def predict_server(file_path: str) -> (str, float):
         ('file', (file_path, file, 'image/jpeg'))
     ]
     logger.debug("immagine caricata dal predict")
-    response = requests.post(HOST_PATH + PORTA_SERVER + PREDICT_FUNCTION, files=files)
+    response = requests.post(f"{HOST_PATH}:{PORTA_SERVER}/{PREDICT_FUNCTION}/", files=files)
     logger.debug("immagine inviata al server")
 
     if response.status_code == 200:
@@ -42,7 +42,7 @@ def grayscale_server(file_path: str) -> (str, float):
     ]
     logger.debug("immagine caricata dal predict")
 
-    response = requests.post(HOST_PATH + PORTA_SERVER + GRAYSCALE_FUNCTION, files=files)
+    response = requests.post(f"{HOST_PATH}:{PORTA_SERVER}/{GRAYSCALE_FUNCTION}/", files=files)
     logger.debug("immagine inviata al server")
 
     if response.status_code == 200:
@@ -52,3 +52,24 @@ def grayscale_server(file_path: str) -> (str, float):
         print('Errore nella richiesta')
         return None
 
+
+def resize_server(file_path: str, altezza, larghezza) -> (str, float):
+
+    file = read_file(SAVE_PATH + file_path)
+
+    payload = {'altezza': altezza, 'larghezza': larghezza}
+
+    files = [
+        ('file', (file_path, file, 'image/jpeg'))
+    ]
+    logger.debug("immagine caricata dal predict")
+
+    response = requests.post(f"{HOST_PATH}:{PORTA_SERVER}/{RESIZE_FUNCTION}/", data = payload, files=files)
+    logger.debug("immagine inviata al server")
+
+    if response.status_code == 200:
+        logger.info('Dati inviati con successo')
+        return response.content
+    else:
+        print('Errore nella richiesta')
+        return None
