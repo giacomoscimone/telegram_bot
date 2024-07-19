fb3ccf7ed9ed5dd16b086a2bfa253e8d25fd8b36
giacomoscimone <giacomo.scimone.it@gmail.com>
2024-07-19 10:00:34 +0200

    update command list

--- commands.py
+++ commands.py
@@ -8,4 +8,6 @@ GRAYSCALE = 'grayscale'
 
 RESIZE = 'resize'
 
-COMMANDS_LIST = [START, HELP, PREDICTION, GRAYSCALE, RESIZE]
+COMMANDS_LIST = [START, f"{HELP} - Mostra la lista dei comandi", f"{PREDICTION} - Esegue la predizione dell'immagine "
+                 f"caricata", f"{GRAYSCALE} - Restituisce l'immagine\ncaricata in scala di grigi", f"{RESIZE} - "
+                 f"Restituisce l'immagine \ncaricata ritagliata, il comando deve \nessere: [/{RESIZE} altezza larghezza]"]
